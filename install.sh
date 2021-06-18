7a4d71162d4a742ce4f6903f6a3610ad91dc3f21
pancho <pancho@localhost.localdomain>
2021-06-18 13:19:19 -0400

    cambioa whitesur theme

--- install.sh
+++ install.sh
@@ -1,34 +1,43 @@
-theme_dir='/usr/share/themes/MateTheme'
+theme_dir="$HOME/.themes/MateTheme"
 
-# este usa la base de un tema que ya existe en mate-desktop
-# el tema de llama 'TraditionalOk'
+dark_color='#1D2126'
+dark_color_hi='#2E363D'
+base_color='#4B7AB3'
+base_color_hi='#578ED1'
 
-sudo mkdir $theme_dir
-sudo cp -rf /usr/share/themes/TraditionalOk/. $theme_dir/
 
-gtk="$theme_dir/gtk-3.0"
+# se nececita 'sassc' para installar 'WhiteSur', y esta en el repo 'raven'
+sudo dnf install 'https://pkgs.dyn.su/el8/base/x86_64/raven-release-1.0-1.el8.noarch.rpm'
+git submodule update --init --recursive
+
+# Installa 'WhiteSur'
+sed -i "s|.*theme_color_default.*|\$theme_color_default: $base_color;|g" ./WhiteSur/src/sass/_colors-palette.scss
+sh './WhiteSur/install.sh'
+# .......
+
+rm -rf $theme_dir
+mv "$HOME/.themes/WhiteSur-light" $theme_dir
+rm -rf $HOME/.themes/WhiteSur*
 
 npm run d
 
-sudo cp './gtk-3.0/main.css' "$gtk/mate_theme.css"
-echo '@import url("mate_theme.css");' | sudo tee -a "$gtk/gtk.css"
+# Modifica algunos datos del tema 'WhiteSur' para adaptarlo a 'MateTheme'
+cp './gtk-3.0/main.css' "$theme_dir/gtk-3.0/mate_theme.css"
+echo '@import url("mate_theme.css");' >> "$theme_dir/gtk-3.0/gtk.css"
 
-sudo sed -i "s|TraditionalOk|MateTheme|g" "$theme_dir/index.theme"
-sudo sed -i "s|IconTheme=mate|IconTheme=Zafiro-icons|g" "$theme_dir/index.theme"
+sed -i "s|WhiteSur-light|MateTheme|g" "$theme_dir/index.theme"
+sed -i "s|IconTheme=WhiteSur|IconTheme=Zafiro-icons|g" "$theme_dir/index.theme"
+
 
 # MetaCity Theme
-sudo cp -rf './metacity-1' $theme_dir
-
-color='#1D2126'
-highlight_color='#2E363D'
-focus_color='#4B7AB3'
-highlight_focus_color='#578ED1'
+rm -rf "$theme_dir/metacity-1"
+cp -rf './metacity-1' $theme_dir
 
 metacity="$theme_dir/metacity-1/metacity-theme-3.xml"
 
-sudo sed -i "s|{color}|$color|g" $metacity
-sudo sed -i "s|{highlight_color}|$highlight_color|g" $metacity
+sed -i "s|{dark_color}|$dark_color|g" $metacity
+sed -i "s|{dark_color_hi}|$dark_color_hi|g" $metacity
 
-sudo sed -i "s|{focus_color}|$focus_color|g" $metacity
-sudo sed -i "s|{highlight_focus_color}|$highlight_focus_color|g" $metacity
+sed -i "s|{base_color}|$base_color|g" $metacity
+sed -i "s|{base_color_hi}|$base_color_hi|g" $metacity
 
