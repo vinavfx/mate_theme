5f46d503e13784ed5e715281445adcd3dcf224b8
pancho <pancho@localhost.localdomain>
2023-01-21 22:16:09 -0500

    compiled

--- install.sh
+++ install.sh
@@ -1,52 +1,8 @@
-teme_dir="$HOME/.themes/MateTheme"
+themes_dir="$HOME/.themes"
 
-dark_color='#1D2126'
-dark_color_hi='#2E363D'
-base_color='#4B7AB3'
-base_color_hi='#578ED1'
-
-npm i
-
-sudo dnf config-manager --set-enabled powertools
-
-# se nececita 'sassc' para installar 'WhiteSur', y esta en el repo 'raven'
-sudo dnf -y install 'https://pkgs.dyn.su/el8/base/x86_64/raven-release-1.0-1.el8.noarch.rpm'
+# Iconos
 git submodule update --init --recursive
-
-# Installa 'WhiteSur'
-palette='./WhiteSur/src/sass/_colors-palette.scss'
-sed -i "s|.*theme_color_default.*|\$theme_color_default: $base_color_hi;|g" $palette
-sed -i "s|.*theme_color_blue.*|\$theme_color_blue: $base_color_hi;|g" $palette
-sh './WhiteSur-icon/install.sh'
 sh './WhiteSur/install.sh'
-# .......
 
-rm -rf $theme_dir
-mv "$HOME/.themes/WhiteSur-light" $theme_dir
-rm -rf $HOME/.themes/WhiteSur*
-
-# Estilo que va por encima de 'WhiteSur' original
-sed -i "s|.*\$base:.*|\$base: $base_color_hi|g" './gtk-3.0/palette.sass'
-npm run d
-#
-
-# Modifica algunos datos del tema 'WhiteSur' para adaptarlo a 'MateTheme'
-cp './gtk-3.0/main.css' "$theme_dir/gtk-3.0/mate_theme.css"
-echo '@import url("mate_theme.css");' >> "$theme_dir/gtk-3.0/gtk.css"
-
-sed -i "s|WhiteSur-light|MateTheme|g" "$theme_dir/index.theme"
-
-# MetaCity Theme
-rm -rf "$theme_dir/metacity-1"
-cp -rf './metacity-1' $theme_dir
-
-metacity="$theme_dir/metacity-1/metacity-theme-3.xml"
-
-sed -i "s|{dark_color}|$dark_color|g" $metacity
-sed -i "s|{dark_color_hi}|$dark_color_hi|g" $metacity
-
-sed -i "s|{base_color}|$base_color|g" $metacity
-sed -i "s|{base_color_hi}|$base_color_hi|g" $metacity
-
-sudo dnf -y erase raven-release
-
+rm -rf $themes_dir/MateTheme
+cp -rf ./MateTheme $themes_dir
--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-
-npm run d
-
-cp ./gtk-3.0/main.css $HOME/.themes/MateTheme/gtk-3.0/mate_theme.css
-
-gsettings set org.mate.interface gtk-theme 'TraditionalOk'
-gsettings set org.mate.interface gtk-theme 'MateTheme'
-
