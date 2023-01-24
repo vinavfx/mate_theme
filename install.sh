a43808564f06531a64a85ac40540586368fcd2f7
Francisco Contreras <fcocc77@gmail.com>
2023-01-23 22:29:11 -0300

    Intercambio de start icon

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 themes_dir="$HOME/.themes"
+icons_dir="$HOME/.local/share/icons"
 
 # Iconos
 git clone 'https://github.com/fcocc77/WhiteSur-icon-theme.git'
 sh './WhiteSur-icon-theme/install.sh'
+
+# Intercambio de icono de start
+for pixel in 16 22 24; do
+    white="$icons_dir/WhiteSur-dark/places/$pixel/start-here.svg"
+    dark="$icons_dir/WhiteSur/places/$pixel/start-here.svg"
+    cp $white $dark
+done
 
 rm -rf $themes_dir/MateTheme
 cp -rf ./MateTheme $themes_dir
